1a747fc7db491c84c4d2e7a4270fe3b98c0e35f3
Prayag Mahesh Sapar <149095595+saparprayag@users.noreply.github.com>
2024-10-20 15:36:39 +0530

    Create dockerhub.groovy

---
+++ vars/dockerhub.groovy
@@ -0,0 +1,7 @@
+def push(String ProjectName, String ImageTag, String dockerHubUser)
+{
+  withCredentials([usernamePassword(credentialsId: "dockerhub", passwordVariable: "dockerHubPass", usernameVariable: "dockerHubUser")]) {
+                    sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+  }
+                    sh "docker push ${dockerHubUser}/${ProjectName}:${ImageTag}"
+}
