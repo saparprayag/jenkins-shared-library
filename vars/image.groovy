d537329d78828546d48181f1d61506433fae0291
Prayag Mahesh Sapar <149095595+saparprayag@users.noreply.github.com>
2024-10-20 15:46:46 +0530

    Update image.groovy

--- vars/image.groovy
+++ vars/image.groovy
@@ -1,4 +1,4 @@
-def build(String ProjectName, String ImageTag, String DockerHubUser)
+def call(String ProjectName, String ImageTag, String DockerHubUser)
 {
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
